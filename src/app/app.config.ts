ea44c55b69a9b2169fc8606ef9b1758cb59ac444
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2025-01-15 16:44:01 +0530

    deps: Angular 19, Primeng 19

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,37 @@ import { provideRouter } from "@angular/router";
 import { ConfirmationService, MessageService } from "primeng/api";
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from "primeng/config";
+import { definePreset } from "@primeng/themes";
 
+const MyPreset = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{blue.50}',
+          100: '{blue.100}',
+          200: '{blue.200}',
+          300: '{blue.300}',
+          400: '{blue.400}',
+          500: '{blue.500}',
+          600: '{blue.600}',
+          700: '{blue.700}',
+          800: '{blue.800}',
+          900: '{blue.900}',
+          950: '{blue.950}'
+      }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), MessageService, ConfirmationService]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(), 
+    providePrimeNG({
+      theme: { preset: MyPreset , options: { darkModeSelector: false }} 
+    }),
+    MessageService,
+    ConfirmationService
+  ]
 };
